47ef08eb5d9fdaf69883948c756db4648da7b41c
YouuDan <wijdan.m.ridwanulloh@gmail.com>
2020-05-24 07:56:11 +0700

    fix env

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -13,13 +13,13 @@ import {
 let githubClientId;
 let githubClientSecret;
 
-if (process.env.NODE_ENV !== 'production') {
-  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-} else {
-  githubClientId = process.env.GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+//   githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+// } else {
+githubClientId = process.env.GITHUB_CLIENT_ID;
+githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+// }
 
 console.log(process.env.NODE_ENV);
 console.log(githubClientId);
